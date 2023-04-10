a72a905b4924423f17d396924b60fdd9aca61cca
Alexander Slepchenko <asander@list.ru>
2023-04-10 09:18:35 +0300

    3. Тестовое задание - проект "Pooh JMS" [#268841]

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -1,8 +1,23 @@
 package ru.job4j.pooh;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class QueueService implements Service {
     @Override
     public Resp process(Req req) {
+        Map<String, ConcurrentLinkedQueue<String>> queue = new ConcurrentHashMap<>();
+        ConcurrentLinkedQueue<String> clq = new ConcurrentLinkedQueue<>();
+        if ("POST".equals(req.httpRequestType())) {
+            queue.putIfAbsent(req.getSourceName(), clq);
+            queue.get(req.getSourceName()).add(
+                    req.httpRequestType() + " " + req.getSourceName() + " " + req.getPoohMode() + " " + req.getParam());
+            return new Resp(req.getParam(), "200");
+        }
+        if ("GET".equals(req.httpRequestType())) {
+           return new Resp(queue.getOrDefault(req.getSourceName(), clq).poll(), "200");
+        }
         return null;
     }
 }
--- src/main/java/ru/job4j/pooh/Req.java
+++ src/main/java/ru/job4j/pooh/Req.java
@@ -1,7 +1,5 @@
 package ru.job4j.pooh;
 
-import java.util.Arrays;
-
 public class Req {
 
     private final String httpRequestType;
