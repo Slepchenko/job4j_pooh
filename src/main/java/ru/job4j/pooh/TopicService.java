86be1686960e8b757eabef49aaab20931a1084d7
Alexander Slepchenko <asander@list.ru>
2023-04-12 22:52:22 +0300

    3. Тестовое задание - проект "Pooh JMS" [#268841]

--- src/main/java/ru/job4j/pooh/TopicService.java
+++ src/main/java/ru/job4j/pooh/TopicService.java
@@ -1,8 +1,28 @@
 package ru.job4j.pooh;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class TopicService implements Service {
     @Override
     public Resp process(Req req) {
+        Map<String, ConcurrentLinkedQueue<String>> topic = new ConcurrentHashMap<>();
+        Map<String, ConcurrentLinkedQueue<String>> ownTopic = new ConcurrentHashMap<>();
+        ConcurrentLinkedQueue<String> clq = new ConcurrentLinkedQueue<>();
+        if ("POST".equals(req.httpRequestType())) {
+            topic.putIfAbsent(req.getSourceName(), new ConcurrentLinkedQueue<>());
+            topic.get(req.getSourceName()).add(
+                    req.httpRequestType() + " " + req.getSourceName() + " " + req.getPoohMode() + " " + req.getParam());
+            return new Resp(req.getParam(), "200");
+        }
+        if ("GET".equals(req.httpRequestType())) {
+
+            ownTopic.putIfAbsent(req.getParam(), new ConcurrentLinkedQueue<>());
+            topic.get(req.getParam()).add(
+                    req.httpRequestType() + " " + req.getSourceName() + " " + req.getPoohMode() + " " + req.getParam());
+
+        }
         return null;
     }
 }
