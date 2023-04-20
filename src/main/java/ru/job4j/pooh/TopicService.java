9894416199c0da5a60d091ebf627b97b439035bb
Alexander Slepchenko <asander@list.ru>
2023-04-20 23:33:54 +0300

    3. Тестовое задание - проект "Pooh JMS" [#268841]

--- src/main/java/ru/job4j/pooh/TopicService.java
+++ src/main/java/ru/job4j/pooh/TopicService.java
@@ -22,21 +22,28 @@ public class TopicService implements Service {
                 .append(req.getParam());
         if ("POST".equals(req.httpRequestType())) {
             if (topics.containsKey(req.getSourceName())) {
-                for (Map.Entry<String, Map<String, ConcurrentLinkedQueue<String>>> r : topics.entrySet()) {
-                    for (String t : r.getValue().get(req.getSourceName())) {
-                    }
+                for (String name : topics.get(req.getSourceName()).keySet()) {
+                    topics.get(req.getSourceName()).get(name).add(req.httpRequestType() + " "
+                            + req.getPoohMode() + " "
+                            + req.getSourceName() + " "
+                            + req.getParam());
+                    System.out.println(topics.get(req.getSourceName()).get(name));
+                    return new Resp(topicStr.toString(), "200");
                 }
             }
+            return new Resp("", "204");
         }
+
         if ("GET".equals(req.httpRequestType())) {
-            if (topics.putIfAbsent(req.getSourceName(), recipients) != null) {
-                topics.get(req.getSourceName()).putIfAbsent(req.getParam(), new ConcurrentLinkedQueue<>());
-                return new Resp(req.getSourceName(), "");
+            if (topics.putIfAbsent(req.getSourceName(), recipients) == null) {
+                topics.get(req.getSourceName()).put(req.getParam(), new ConcurrentLinkedQueue<>());
+
+//                return new Resp(Req.of(topicStr.toString()).getParam(), "200");
             }
-            if (topics.get(req.getSourceName()).putIfAbsent(req.getParam(), new ConcurrentLinkedQueue<>()) != null) {
-                return new Resp(topics.get(req.getSourceName()).get(req.getParam()).poll(), "200");
-            }
-            return new Resp(topics.get(req.getSourceName()).get(req.getParam()).poll(), "200");
+            topics.get(req.getSourceName()).putIfAbsent(req.getParam(), new ConcurrentLinkedQueue<>());
+//                return new Resp(topics.get(req.getSourceName()).get(req.getParam()).poll(), "200");
+
+            return new Resp(, "200");
         }
         return new Resp("", "204");
     }
